707f3735b641da14f8436f5071363deb202ff6fe
antfriend <antfriend@gmail.com>
2024-02-23 06:41:49 -0700

    try add meta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits, HitsPerPage, PoweredBy, Configure } from 'react-instantsearch';
+import { InstantSearch, SearchBox, Hits, PoweredBy, Configure } from 'react-instantsearch';
 import './App.css'; // Importing the CSS file
 import Eyeball from 'react-eyeball';
 
-// Replace with your own Algolia credentials
+
 const searchClient = algoliasearch('3AUHR4IE0C', 'b152fa9673ced6bc795ad370b729bfd2');
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
@@ -12,7 +12,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 });
 
 let some_value = params.q || 'crepuscular rays';
-// Define how each hit (result) will be rendered
+
 const Hit = ({ hit }) => (
   <div className="hit-card"> 
     <h1>{hit.title}</h1>
@@ -35,12 +35,9 @@ function App() {
           query: some_value
         },
       }}
-
     >
-        <Configure
-        analytics={true}
-        hitsPerPage={1}
-      />
+
+    <Configure  analytics={true}  hitsPerPage={1}  />
       
       <PoweredBy theme="dark" style={{ width: "200px" }}/>
 
@@ -55,12 +52,7 @@ function App() {
 
       </div>
       
-      <HitsPerPage
-          items={[
-            { label: '1 hit per page', value: 1, default: true },
-            { label: '3 hits per page', value: 3 },
-          ]}
-        />
+
         
     </InstantSearch>
   );
